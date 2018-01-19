ddfbcf5cb3c2934accc41eb47a5c9cafd27fb041
Samuel Grossman <samuelgr@stanford.edu>
2018-01-18 16:19:55 -0800

    Added a simple dynamic scheduler.

--- include/parutil.h
+++ include/parutil.h
@@ -36,133 +36,156 @@ typedef enum EParutilStaticScheduler
 } EParutilStaticScheduler;
 
 
-// -------- FUNCTIONS ------------------------------------------------------ //
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-    // -------- ATOMIC ----------------------------------------------------- //
-    
-    /// Performs an atomic add operation with 8-bit operands.
-    /// The specified memory location is updated with its current value plus the supplied value.
-    /// @param [in,out] ptr Memory location of the value to add. This location is updated with the result of the summation.
-    /// @param [in] incr Value to add to the specified memory location.
-    void parutilAtomicAdd8(uint8_t* const ptr, const uint8_t incr);
+// -------- FUNCTIONS: ATOMIC ---------------------------------------------- //
 
-    /// Performs an atomic add operation with 16-bit operands.
-    /// The specified memory location is updated with its current value plus the supplied value.
-    /// @param [in,out] ptr Memory location of the value to add. This location is updated with the result of the summation.
-    /// @param [in] incr Value to add to the specified memory location.
-    void parutilAtomicAdd16(uint16_t* const ptr, const uint16_t incr);
-    
-    /// Performs an atomic add operation with 32-bit operands.
-    /// The specified memory location is updated with its current value plus the supplied value.
-    /// @param [in,out] ptr Memory location of the value to add. This location is updated with the result of the summation.
-    /// @param [in] incr Value to add to the specified memory location.
-    void parutilAtomicAdd32(uint32_t* const ptr, const uint32_t incr);
-    
-    /// Performs an atomic add operation with 64-bit operands.
-    /// The specified memory location is updated with its current value plus the supplied value.
-    /// @param [in,out] ptr Memory location of the value to add. This location is updated with the result of the summation.
-    /// @param [in] incr Value to add to the specified memory location.
-    void parutilAtomicAdd64(uint64_t* const ptr, const uint64_t incr);
-    
-    /// Performs an atomic exchange operation with 8-bit operands.
-    /// The specified memory location is exchanged with the specified value.
-    /// @param [in,out] ptr Memory location of the value to be exchanged.
-    /// @param [in] value Value to be written to the memory location.
-    /// @return Value at the memory location prior to the exchange operation.
-    uint8_t parutilAtomicExchange8(uint8_t* const ptr, const uint8_t value);
+/// Performs an atomic add operation with 8-bit operands.
+/// The specified memory location is updated with its current value plus the supplied value.
+/// @param [in,out] ptr Memory location of the value to add. This location is updated with the result of the summation.
+/// @param [in] incr Value to add to the specified memory location.
+void parutilAtomicAdd8(uint8_t* const ptr, const uint8_t incr);
 
-    /// Performs an atomic exchange operation with 16-bit operands.
-    /// The specified memory location is exchanged with the specified value.
-    /// @param [in,out] ptr Memory location of the value to be exchanged.
-    /// @param [in] value Value to be written to the memory location.
-    /// @return Value at the memory location prior to the exchange operation.
-    uint16_t parutilAtomicExchange16(uint16_t* const ptr, const uint16_t value);
-    
-    /// Performs an atomic exchange operation with 32-bit operands.
-    /// The specified memory location is exchanged with the specified value.
-    /// @param [in,out] ptr Memory location of the value to be exchanged.
-    /// @param [in] value Value to be written to the memory location.
-    /// @return Value at the memory location prior to the exchange operation.
-    uint32_t parutilAtomicExchange32(uint32_t* const ptr, const uint32_t value);
-    
-    /// Performs an atomic exchange operation with 64-bit operands.
-    /// The specified memory location is exchanged with the specified value.
-    /// @param [in,out] ptr Memory location of the value to be exchanged.
-    /// @param [in] value Value to be written to the memory location.
-    /// @return Value at the memory location prior to the exchange operation.
-    uint64_t parutilAtomicExchange64(uint64_t* const ptr, const uint64_t value);
-    
-    /// Performs an atomic exchange-and-add operation with 8-bit operands.
-    /// The specified memory location is updated with its current value plus the supplied value, and the old value of the memory location is returned.
-    /// @param [in,out] ptr Memory location of the value to add. This location is updated with the result of the summation.
-    /// @param [in] incr Value to add to the specified memory location.
-    /// @return Value at the memory location prior to the addition operation.
-    uint8_t parutilAtomicExchangeAdd8(uint8_t* const ptr, const uint8_t incr);
+/// Performs an atomic add operation with 16-bit operands.
+/// The specified memory location is updated with its current value plus the supplied value.
+/// @param [in,out] ptr Memory location of the value to add. This location is updated with the result of the summation.
+/// @param [in] incr Value to add to the specified memory location.
+void parutilAtomicAdd16(uint16_t* const ptr, const uint16_t incr);
 
-    /// Performs an atomic exchange-and-add operation with 16-bit operands.
-    /// The specified memory location is updated with its current value plus the supplied value, and the old value of the memory location is returned.
-    /// @param [in,out] ptr Memory location of the value to add. This location is updated with the result of the summation.
-    /// @param [in] incr Value to add to the specified memory location.
-    /// @return Value at the memory location prior to the addition operation.
-    uint16_t parutilAtomicExchangeAdd16(uint16_t* const ptr, const uint16_t incr);
-    
-    /// Performs an atomic exchange-and-add operation with 32-bit operands.
-    /// The specified memory location is updated with its current value plus the supplied value, and the old value of the memory location is returned.
-    /// @param [in,out] ptr Memory location of the value to add. This location is updated with the result of the summation.
-    /// @param [in] incr Value to add to the specified memory location.
-    /// @return Value at the memory location prior to the addition operation.
-    uint32_t parutilAtomicExchangeAdd32(uint32_t* const ptr, const uint32_t incr);
-    
-    /// Performs an atomic exchange-and-add operation with 64-bit operands.
-    /// The specified memory location is updated with its current value plus the supplied value, and the old value of the memory location is returned.
-    /// @param [in,out] ptr Memory location of the value to add. This location is updated with the result of the summation.
-    /// @param [in] incr Value to add to the specified memory location.
-    /// @return Value at the memory location prior to the addition operation.
-    uint64_t parutilAtomicExchangeAdd64(uint64_t* const ptr, const uint64_t incr);
-    
-    
-    // -------- MEMORY ----------------------------------------------------- //
-    
-    /// Copies `num` bytes of memory at `source` to memory at `destination`.
-    /// Intended to be a drop-in replacement for the standard `memcpy()` function.
-    /// It is the caller's responsibility to ensure that `source` and `destination` regions do not overlap.
-    /// If called from within a Spindle parallelized region, every thread in the same task must invoke this function with the same arguments.
-    /// If not, uses all available hardware threads on the NUMA node of the destination buffer.
-    /// Reverts to standard `memcpy()` if `num` is small enough.
-    /// @param [in] destination Target memory buffer.
-    /// @param [in] source Source memory buffer.
-    /// @param [in] num Number of bytes to copy.
-    /// @return `destination` is returned upon completion.
-    void* parutilMemoryCopy(void* destination, const void* source, size_t num);
+/// Performs an atomic add operation with 32-bit operands.
+/// The specified memory location is updated with its current value plus the supplied value.
+/// @param [in,out] ptr Memory location of the value to add. This location is updated with the result of the summation.
+/// @param [in] incr Value to add to the specified memory location.
+void parutilAtomicAdd32(uint32_t* const ptr, const uint32_t incr);
 
-    /// Sets `num` bytes of memory at `buffer` to the value specified by `value`.
-    /// Intended to be a drop-in replacement for the standard `memset()` function.
-    /// If called from within a Spindle parallelized region, every thread in the same task must invoke this function with the same arguments.
-    /// If not, uses all available hardware threads on the NUMA node of the destination buffer.
-    /// Reverts to standard `memset()` if `num` is small enough.
-    /// @param [in] buffer Target memory buffer.
-    /// @param [in] value Byte-sized value to write to the target memory buffer.
-    /// @param [in] num Number of bytes to initialize.
-    /// @return `buffer` is returned upon completion.
-    void* parutilMemorySet(void* buffer, uint8_t value, size_t num);
-    
-    
-    // -------- SCHEDULER -------------------------------------------------- //
-    
-    /// Uses a static scheduler of the specified type to provide the caller with information on assigned work.
-    /// Intended to be called within a Spindle parallelized region and will fail otherwise.
-    /// Each thread within a Spindle task should call this function.
-    /// The information provided via the output #SParutilStaticSchedule can be used to determine which units of parallel work should be performed by each thread.
-    /// For example, when parallelizing a `for` loop, the thread could start with `startUnit`, compare for less-than with `endUnit`, and increment by `increment`.
-    /// @param [in] type Type of static scheduler to use.
-    /// @param [in] units Total number of units of work that need to be scheduled.
-    /// @param [out] schedule Scheduling information, provided as output.
-    /// @return `true` if successful (i.e. in a parallel region, output parameter is not `NULL`, and so on), `false` otherwise.
-    bool parutilSchedulerStatic(const EParutilStaticScheduler type, const uint64_t units, SParutilStaticSchedule* const schedule);
-    
+/// Performs an atomic add operation with 64-bit operands.
+/// The specified memory location is updated with its current value plus the supplied value.
+/// @param [in,out] ptr Memory location of the value to add. This location is updated with the result of the summation.
+/// @param [in] incr Value to add to the specified memory location.
+void parutilAtomicAdd64(uint64_t* const ptr, const uint64_t incr);
+
+/// Performs an atomic exchange operation with 8-bit operands.
+/// The specified memory location is exchanged with the specified value.
+/// @param [in,out] ptr Memory location of the value to be exchanged.
+/// @param [in] value Value to be written to the memory location.
+/// @return Value at the memory location prior to the exchange operation.
+uint8_t parutilAtomicExchange8(uint8_t* const ptr, const uint8_t value);
+
+/// Performs an atomic exchange operation with 16-bit operands.
+/// The specified memory location is exchanged with the specified value.
+/// @param [in,out] ptr Memory location of the value to be exchanged.
+/// @param [in] value Value to be written to the memory location.
+/// @return Value at the memory location prior to the exchange operation.
+uint16_t parutilAtomicExchange16(uint16_t* const ptr, const uint16_t value);
+
+/// Performs an atomic exchange operation with 32-bit operands.
+/// The specified memory location is exchanged with the specified value.
+/// @param [in,out] ptr Memory location of the value to be exchanged.
+/// @param [in] value Value to be written to the memory location.
+/// @return Value at the memory location prior to the exchange operation.
+uint32_t parutilAtomicExchange32(uint32_t* const ptr, const uint32_t value);
+
+/// Performs an atomic exchange operation with 64-bit operands.
+/// The specified memory location is exchanged with the specified value.
+/// @param [in,out] ptr Memory location of the value to be exchanged.
+/// @param [in] value Value to be written to the memory location.
+/// @return Value at the memory location prior to the exchange operation.
+uint64_t parutilAtomicExchange64(uint64_t* const ptr, const uint64_t value);
+
+/// Performs an atomic exchange-and-add operation with 8-bit operands.
+/// The specified memory location is updated with its current value plus the supplied value, and the old value of the memory location is returned.
+/// @param [in,out] ptr Memory location of the value to add. This location is updated with the result of the summation.
+/// @param [in] incr Value to add to the specified memory location.
+/// @return Value at the memory location prior to the addition operation.
+uint8_t parutilAtomicExchangeAdd8(uint8_t* const ptr, const uint8_t incr);
+
+/// Performs an atomic exchange-and-add operation with 16-bit operands.
+/// The specified memory location is updated with its current value plus the supplied value, and the old value of the memory location is returned.
+/// @param [in,out] ptr Memory location of the value to add. This location is updated with the result of the summation.
+/// @param [in] incr Value to add to the specified memory location.
+/// @return Value at the memory location prior to the addition operation.
+uint16_t parutilAtomicExchangeAdd16(uint16_t* const ptr, const uint16_t incr);
+
+/// Performs an atomic exchange-and-add operation with 32-bit operands.
+/// The specified memory location is updated with its current value plus the supplied value, and the old value of the memory location is returned.
+/// @param [in,out] ptr Memory location of the value to add. This location is updated with the result of the summation.
+/// @param [in] incr Value to add to the specified memory location.
+/// @return Value at the memory location prior to the addition operation.
+uint32_t parutilAtomicExchangeAdd32(uint32_t* const ptr, const uint32_t incr);
+
+/// Performs an atomic exchange-and-add operation with 64-bit operands.
+/// The specified memory location is updated with its current value plus the supplied value, and the old value of the memory location is returned.
+/// @param [in,out] ptr Memory location of the value to add. This location is updated with the result of the summation.
+/// @param [in] incr Value to add to the specified memory location.
+/// @return Value at the memory location prior to the addition operation.
+uint64_t parutilAtomicExchangeAdd64(uint64_t* const ptr, const uint64_t incr);
+
+
+// -------- FUNCTIONS: MEMORY ---------------------------------------------- //
+
+/// Copies `num` bytes of memory at `source` to memory at `destination`.
+/// Intended to be a drop-in replacement for the standard `memcpy()` function.
+/// It is the caller's responsibility to ensure that `source` and `destination` regions do not overlap.
+/// If called from within a Spindle parallelized region, every thread in the same task must invoke this function with the same arguments.
+/// If not, uses all available hardware threads on the NUMA node of the destination buffer.
+/// Reverts to standard `memcpy()` if `num` is small enough.
+/// @param [in] destination Target memory buffer.
+/// @param [in] source Source memory buffer.
+/// @param [in] num Number of bytes to copy.
+/// @return `destination` is returned upon completion.
+void* parutilMemoryCopy(void* destination, const void* source, size_t num);
+
+/// Sets `num` bytes of memory at `buffer` to the value specified by `value`.
+/// Intended to be a drop-in replacement for the standard `memset()` function.
+/// If called from within a Spindle parallelized region, every thread in the same task must invoke this function with the same arguments.
+/// If not, uses all available hardware threads on the NUMA node of the destination buffer.
+/// Reverts to standard `memset()` if `num` is small enough.
+/// @param [in] buffer Target memory buffer.
+/// @param [in] value Byte-sized value to write to the target memory buffer.
+/// @param [in] num Number of bytes to initialize.
+/// @return `buffer` is returned upon completion.
+void* parutilMemorySet(void* buffer, uint8_t value, size_t num);
+
+
+// -------- FUNCTIONS: SCHEDULER ------------------------------------------- //
+
+/// Uses a static scheduler of the specified type to provide the caller with information on assigned work.
+/// Intended to be called within a Spindle parallelized region and will fail otherwise.
+/// Each thread within a Spindle task should call this function with the same values for the first two parameters.
+/// The information provided via the output #SParutilStaticSchedule can be used to determine which units of parallel work should be performed by each thread.
+/// For example, when parallelizing a `for` loop, the thread could start with `startUnit`, compare for less-than with `endUnit`, and increment by `increment`.
+/// @param [in] type Type of static scheduler to use.
+/// @param [in] units Total number of units of work that need to be scheduled.
+/// @param [out] schedule Scheduling information, provided as output.
+/// @return `true` if successful (i.e. in a parallel region, output parameter is not `NULL`, and so on), `false` otherwise.
+bool parutilSchedulerStatic(const EParutilStaticScheduler type, const uint64_t units, SParutilStaticSchedule* const schedule);
+
+/// Initializes a dynamic scheduler to assign work to the caller and allocates required memory.
+/// A dynamic scheduler assigns work to threads in the order that they become available to take on more work.
+/// Intended to be called within a Spindle parallelized region and will fail otherwise.
+/// Each thread within a Spindle task should call this function with the same value for the first parameter.
+/// @param [in] numUnits Total number of units of work that need to be scheduled.
+/// @param [out] schedule Pointer to a variable that will hold the dynamic scheduler handle used to identify the scheduler instance, or `NULL` on failure.
+/// @return First unit of work to be performed by the caller, or `UINT64_MAX` if no work is available.
+uint64_t parutilSchedulerDynamicInit(const uint64_t numUnits, void** schedule);
+
+/// Obtains the next unit of work to be assigned to the calling thread.
+/// Intended to be called within a Spindle parallelized region and will fail otherwise.
+/// Threads within a Spindle task should call this function with the same value as received back from #parutilSchedulerDynamicInit.
+/// @param [in] Handle used to identify the dynamic scheduler instance.
+/// @return Next unit of work to be performed by the caller, or `UINT64_MAX` if no work is available.
+uint64_t parutilSchedulerDynamicGetWork(void* schedule);
+
+/// Destroys the specified dynamic scheduler once no further work is available.
+/// Intended to be called within a Spindle parallelized region and will fail otherwise.
+/// Each thread within a Spindle task should call this function with the same value as received back from #parutilSchedulerDynamicInit.
+/// All required synchronization is handled internally; there is no need for the caller to do any synchronization.
+/// @param [in] Handle used to identify the dynamic scheduler instance.
+void parutilSchedulerDynamicExit(void* schedule);
+
+
 #ifdef __cplusplus
 }
 #endif
--- source/scheduler.c
+++ source/scheduler.c
@@ -12,10 +12,20 @@
 
 #include "../parutil.h"
 
+#include <silo.h>
 #include <spindle.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+
+
+// -------- TYPE DEFINITIONS ----------------------------------------------- //
+
+typedef struct SParutilDynamicSchedule
+{
+    uint64_t currentUnit;                                                   ///< Current unit of work to be assigned.
+    uint64_t numUnits;                                                      ///< Total number of units of work.
+} SParutilDynamicSchedule;
 
 
 // -------- INTERNAL FUNCTIONS --------------------------------------------- //
@@ -49,4 +59,85 @@ bool parutilSchedulerStatic(const EParutilStaticScheduler type, const uint64_t u
     }
     
     return true;
+}
+
+// --------
+
+uint64_t parutilSchedulerDynamicInit(const uint64_t numUnits, void** schedule)
+{
+    if (spindleIsInParallelRegion())
+    {
+        SParutilDynamicSchedule* scheduleBuf = NULL;
+
+        if (0 == spindleGetLocalThreadID())
+        {
+            // First thread allocates, initializes, and shares the dynamic scheduler object.
+            scheduleBuf = (SParutilDynamicSchedule*)siloSimpleBufferAllocLocal(sizeof(SParutilDynamicSchedule));
+
+            if (NULL != scheduleBuf)
+            {
+                scheduleBuf->currentUnit = (uint64_t)spindleGetLocalThreadCount();
+                scheduleBuf->numUnits = numUnits;
+            }
+
+            spindleDataShareSendLocal((uint64_t)scheduleBuf);
+        }
+        else
+        {
+            // All other threads wait for the address of the dynamic scheduler object.
+            scheduleBuf = (SParutilDynamicSchedule*)spindleDataShareReceiveLocal();
+        }
+
+        *schedule = (void*)scheduleBuf;
+        
+        if (NULL == scheduleBuf)
+            return UINT64_MAX;
+
+        // First unit of work is just the current thread's local identifier.
+        const uint64_t firstWorkUnit = (uint64_t)spindleGetLocalThreadID();
+
+        if (firstWorkUnit < scheduleBuf->numUnits)
+            return firstWorkUnit;
+        else
+            return UINT64_MAX;
+    }
+    else
+    {
+        *schedule = NULL;
+        return UINT64_MAX;
+    }
+}
+
+// --------
+
+uint64_t parutilSchedulerDynamicGetWork(void* schedule)
+{
+    // Check pre-conditions for this function.
+    if ((!spindleIsInParallelRegion()) || (NULL == schedule))
+        return UINT64_MAX;
+
+    // Get the next unit of work for this thread.
+    SParutilDynamicSchedule* const scheduleBuf = (SParutilDynamicSchedule*)schedule;
+
+    const uint64_t nextWorkUnit = parutilAtomicExchangeAdd64(&scheduleBuf->currentUnit, 1ull);
+
+    if (nextWorkUnit < scheduleBuf->numUnits)
+        return nextWorkUnit;
+    else
+        return UINT64_MAX;
+}
+
+// --------
+
+void parutilSchedulerDynamicExit(void* schedule)
+{
+    // Check pre-conditions for this function.
+    if ((!spindleIsInParallelRegion()) || (NULL == schedule))
+        return;
+
+    spindleBarrierLocal();
+
+    // First thread frees the previously-allocated dynamic scheduler object.
+    if (0 == spindleGetLocalThreadID)
+        siloFree(schedule);
 }
